982d688bdcd9b6ff4886646f75a9db61dc17a539
SagarKapase <97930178+SagarKapase@users.noreply.github.com>
2023-07-11 16:21:23 +0530

    initial commit

---
+++ image-search-app/js/index.js
@@ -0,0 +1,6 @@
+const accessKey = "8ettHU0gWodwGQRxW_6uS7finJpDtYF7KHyXahZdvOs";
+const formE1 = document.querySelector("from");
+const searchInputE1 = document.getElementById("search-input");
+const searchResultsE1 = document.querySelector(".search-results");
+const showMoreButtonE1 = document.getElementById("show-more-button");
+
